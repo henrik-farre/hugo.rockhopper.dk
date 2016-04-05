74153e6cda7543c7fb65b2b3c3d092da7c43ca21
Henrik Farre <henrik@bellcom.dk>
2016-04-05 14:01:38 +0200

    Build drafts when developing

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf public/*
-hugo --theme=rockhopper --baseUrl="http://hugo.rockhopper.hf/" -w
+hugo --theme=rockhopper --baseUrl="http://hugo.rockhopper.hf/" -w --buildDrafts=true
